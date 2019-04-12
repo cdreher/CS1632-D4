1c08ae1b701ab57915ca054b063114824028a18d
Collin Dreher <collin.dreher@gmail.com>
2019-04-12 18:03:49 -0400

    Check out of order blocks

--- simulator.rb
+++ simulator.rb
@@ -19,4 +19,22 @@ class VerifierSimulator
     return false unless File.file?(args[0])
     true
   end
+
+  # Perform hash calculation of string
+  def hash(s)
+    total = 0
+    characters = s.split('')
+    characters.each do |x|
+      x = x.unpack('U*')[0]
+      total += ((x**3000) + (x**x) - (3**x)) * (7**x)
+    end
+    (total & 65536).to_s(16)
+  end
+
+  # Check order of blocks
+  def out_of_order(curr, prev)
+    return true unless (curr - prev) == 1
+    false
+  end
+
 end
--- verifier_runner.rb
+++ verifier_runner.rb
@@ -3,6 +3,7 @@ require_relative 'simulator.rb'
 # Runner class -> essentially run the Billcoin simulator
 class VerifierRunner
   def initialize
+    @previous_block = -1
     @sim = VerifierSimulator.new
   end
 
@@ -11,12 +12,30 @@ class VerifierRunner
     if @sim.check(args) && @sim.isFile(args)
       filename = args[0]
       file = File.open filename
-      puts "File IS VALID"
+      parse(file)
       file.close
     else
       puts 'File is invalid.'
       exit 1
     end
+  end
+
+  # Parse billcoin input file
+  def parse(file)
+    while(line = file.gets)
+      blockchain = line.split('|')
+      s = blockchain[0] + '|' + blockchain[1] + '|' + blockchain[2] + '|' + blockchain[3]
+      hashed = @sim.hash(s)
+
+      check_order(blockchain)
+    end
+  end
+
+  # Check if blocks are in order
+  def check_order(blockchain)
+    return unless @sim.out_of_order(blockchain[0].to_i, @previous_block.to_i)
+    puts 'Invalid block number ' + blockchain[0] + ', should be ' + (@previous_block.to_i + 1).to_s
+    exit 1
   end
 
 end
