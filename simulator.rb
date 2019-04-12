c88fb72f8af94406d653ce6e7eb54dfe8e962105
Collin Dreher <collin.dreher@gmail.com>
2019-04-12 18:34:46 -0400

    Set up begin steps of checks for time and hash

--- simulator.rb
+++ simulator.rb
@@ -28,12 +28,34 @@ class VerifierSimulator
       x = x.unpack('U*')[0]
       total += ((x**3000) + (x**x) - (3**x)) * (7**x)
     end
-    (total & 65536).to_s(16)
+    (total % 65536).to_s(16)
   end
 
   # Check order of blocks
   def out_of_order(curr, prev)
     return true unless (curr - prev) == 1
+    false
+  end
+
+  # Check the timestamp on each individula block
+  # Return 1 if all is good, return 0 if not.
+  def check_time(curr, prev)
+    curr_time = curr.split('.')[0]
+    prev_time = prev.split('.')[0]
+    curr_nano = curr_time.split('.')[1]
+    prev_nano = prev_time.split('.')[1]
+
+    return 1 if (curr_time.to_i - prev_time.to_i) > 0
+
+    if (curr_time.to_i - prev_time.to_i).zero?
+      return 1 unless (curr_nano.to_i - prev_nano.to_i) <= 0
+    end
+    0
+  end
+
+  # Check hash
+  def check_hash(calc, curr)
+    return true unless calc.eql? curr
     false
   end
 
--- verifier_runner.rb
+++ verifier_runner.rb
@@ -4,6 +4,8 @@ require_relative 'simulator.rb'
 class VerifierRunner
   def initialize
     @previous_block = -1
+    @previous_time = 0
+    @previous_hash = '0'
     @sim = VerifierSimulator.new
   end
 
@@ -28,6 +30,9 @@ class VerifierRunner
       hashed = @sim.hash(s)
 
       check_order(blockchain)
+      check_time(blockchain)
+      check_hash(blockchain)
+
     end
   end
 
@@ -35,6 +40,22 @@ class VerifierRunner
   def check_order(blockchain)
     return unless @sim.out_of_order(blockchain[0].to_i, @previous_block.to_i)
     puts 'Invalid block number ' + blockchain[0] + ', should be ' + (@previous_block.to_i + 1).to_s
+    exit 1
+  end
+
+  # Check the timestamp
+  def check_time(blockchain)
+    return unless @sim.check_time(blockchain[3].to_s, @previous_time.to_s) != 1 && @previous_time != 0
+    puts 'Line ' + blockchain[0] + ': Previous timestamp ' + @previous_time.to_s
+    puts ' >= new timestamp ' + blockchain[3].to_s
+    exit 1
+  end
+
+  # Check previous hash
+  def check_hash(blockchain)
+    return unless @sim.check_hash(blockchain[1], @previous_hash) && @previous_hash != '0'
+    puts 'Previous hash was ' + blockchain[1] + ', should be ' + @previous_hash.to_s
+    puts 'BLOCKCHAIN INVALID'
     exit 1
   end
 
